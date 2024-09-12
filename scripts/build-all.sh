9c6e3bc04b1b25a0adca20d44723a877285b3a09
Francesco Ferrari <62111448+flakyferr@users.noreply.github.com>
2024-09-13 00:25:23 +0200

    Update build-all.sh

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -264,6 +264,8 @@ if ! ${rootdir}/scripts/avr32-versions.sh ${rootdir} ${autocheckout} \
 then
     echo "ERROR: Failed to checkout GIT versions of tools"
     echo "- see ${logfile}"
+    echo "LOGFILE: "
+    cat ${logfile}
     exit 1
 fi
 
